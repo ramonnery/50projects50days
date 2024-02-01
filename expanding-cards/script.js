37b8dd48c6d23e75911ef3e2838d6f35062e2f61
ramonnery <ramonnery2020@gmail.com>
2024-02-01 13:22:15 -0300

    Terreno preparado para lógica com JS

---
+++ expanding-cards/script.js
@@ -0,0 +1,9 @@
+const images = document.querySelectorAll('.images')
+
+images.forEach(img => {
+    console.log(img)
+    img.addEventListener('click', e => {
+        // Ainda penando pra pensar na lógica
+    })          
+})
+
