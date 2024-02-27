cb3f8d27f1edbd7ff69eb2746331b886be7b4dfb
ramonnery <ramonnery2020@gmail.com>
2024-02-27 13:11:31 -0300

    ...

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,10 +1,14 @@
-const stepContainer = document.querySelector('.progress-bar')
+const progressBar = document.querySelector('.step-container')
 const steps = document.querySelectorAll('.steps')
 const prevBtn = document.querySelector('#prev')
 const nextBtn = document.querySelector('#next')
+const progressClasses = ['progress-bar', 'progress-bar-start', 'progress-bar-mid', 'progress-bar-end']    
 
-console.log(stepContainer)
-    
+function increaseProgress(index) {
+    let newClass = progressClasses[index]
+    let oldClass = progressBar.classList[1]
+    progressBar.classList.replace(oldClass, newClass)
+}
 
 prevBtn.addEventListener('click', e => {
     let numberOfIndexes = steps.length - 1
@@ -12,7 +16,7 @@ prevBtn.addEventListener('click', e => {
 
     for (numberOfIndexes; numberOfIndexes > 0; numberOfIndexes--) {
         let step = steps[numberOfIndexes]
-        
+
         if(numberOfIndexes === 1) prevBtn.classList.add('btn-disabled')
         
         if(step.classList.contains('step-abled')) {
@@ -28,8 +32,10 @@ nextBtn.addEventListener('click', e => {
     for (let step of steps) {
         if(!step.classList.contains('step-abled')) {
             step.classList.add('step-abled')
+            console.log(step.id)
+            increaseProgress(Number(step.id))
 
-            if(step.id === 'step4') nextBtn.classList.add('btn-disabled')
+            if(step.id === '3') nextBtn.classList.add('btn-disabled')
 
             break
         }
